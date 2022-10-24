ef688b9e8baf9e4a26541241243dad6e9076f0e5
Robert B <robertb@lifesciencepublishers.com>
2022-10-24 11:56:41 -0600

    Youtube Downloader

--- YouTubeDownloader.py
+++ YouTubeDownloader.py
@@ -1,13 +1,21 @@
 # https://pytube.io/en/latest/
 from pytube import YouTube
+import os 
+
 
 link = input("Add YouTube Link: ")
 yt = YouTube(link)
 
-# yd = yt.streams.get_highest_resolution()
+yd = yt.streams.get_highest_resolution()
 yd = yt.streams.get_by_itag(22)
 print(yt.title)
 print(yd)
+folderName = 'videos'
 
-# ADD FOLDER HERE
-yd.download('./')
+if not os.path.exists(folderName):
+    os.mkdir(folderName)
+    # ADD FOLDER HERE
+    yd.download(folderName)
+else:
+    # ADD FOLDER HERE
+    yd.download(folderName)
